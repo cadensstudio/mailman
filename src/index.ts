07ca6819814acba46052b384d0f911b787b2623c
Caden <54109914+cadensstudio@users.noreply.github.com>
2024-06-05 06:02:59 -0700

    feat: send mail from contact form entry works (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,44 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { EmailMessage } from 'cloudflare:email';
+import { createMimeMessage } from 'mimetext';
+
+export interface Env {
+	SEB: any;
+	SENDER: string;
+	RECIPIENT: string;
+}
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env): Promise<Response> {
+		if (request.method === 'POST') {
+			const formData = await request.formData();
+			const firstName = formData.get('firstName') as string;
+			const lastName = formData.get('lastName') as string;
+			const email = formData.get('email') as string;
+			const reason = formData.get('reason') as string;
+
+			const msg = createMimeMessage();
+			msg.setSender({ name: 'Website Bot', addr: env.SENDER });
+			msg.setRecipient(env.RECIPIENT);
+			msg.setSubject('New Contact Form Submission');
+			msg.addMessage({
+				contentType: 'text/plain',
+				data: `Name: ${firstName} ${lastName}\nEmail: ${email}\nMessage: ${reason}`,
+			});
+
+			const message = new EmailMessage(env.SENDER, env.RECIPIENT, msg.asRaw());
+
+			try {
+				await env.SEB.send(message);
+				return new Response('Email sent successfully!', { status: 200 });
+			} catch (e) {
+				if (e instanceof Error) {
+					return new Response(`Failed to send email: ${e.message}`, { status: 500 });
+				} else {
+					return new Response('An unknown error occurred', { status: 500 });
+				}
+			}
+		} else {
+			return new Response('Method not allowed', { status: 405 });
+		}
 	},
 };
